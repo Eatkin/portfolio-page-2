5d6bc974157ff5df7a1a07150f938eb0ef3a76ad
Ed <edward.atkin@live.com>
2026-01-24 17:02:20 +0000

    feat (all): Full site overhaul: content updates, jinja2 now used for templating, style/structure overhaul

--- build.py
+++ build.py
@@ -1,89 +1,90 @@
 import os
-import re
+from typing import Any
+from typing import Dict
+
+import yaml
+from jinja2 import Environment
+from jinja2 import FileSystemLoader
 from marko import convert
-from yaml import safe_load
 
-def load_content(file):
-    with open(file, 'r') as f:
-        return safe_load(f)
+# Singular massive selection of emojis for chaos
+EMOJIS = [
+    "☠️",
+    "🔥",
+    "✨",
+    "🤖",
+    "🎨",
+    "📖",
+    "🚀",
+    "💾",
+    "🎭",
+    "💃",
+    "🛠",
+    "⚙️",
+    "🧰",
+    "🧨",
+    "⚡",
+    "ϟ",
+    "☣️",
+    "🩸",
+    "🖤",
+    "🗡",
+    "⚔️",
+    "🚧",
+    "📦",
+    "📎",
+    "✎",
+    "🖋",
+    "🧪",
+    "🧬",
+    "📐",
+    "📈",
+    "🎯",
+    "🏴",
+    "⏳",
+    "♻️",
+    "🧠",
+    "🤯",
+    "🐍",
+    "👾",
+    "🐙",
+    "🕷",
+    "🧱",
+    "📡",
+    "🛰",
+    "🧿",
+]
 
-def load_template(file):
-    with open(file, 'r') as f:
-        return f.read()
 
-def build_page(content, template):
-    html = []
-    for k, v in content.items():
-        if k == "head":
-            template = build_head(content, template)
-        elif k == "landing":
-            template = build_header(content, template)
-        else:
-            if v['title']:
-                html.append(f'<section id="{k}" class="section-head"><h2>{v["title"]}</h2>')
-            for i, item in enumerate(v['sections']):
-                temp = []
-                temp.append(f'<section class="{k}">')
-                if item['name']:
-                    temp.append(f"<h3>{item['name']}</h3>")
-                temp.append(convert(item['content']))
-                temp.append("</section>")
-                # Trim the sections tag if we're using inline html
-                if '<section ' in item['content']:
-                    temp = temp[1:-1]
-                elif i == 0:
-                    temp = temp[1:]
+def markdown_filter(text: str) -> str:
+    return convert(text)
 
-                html.extend(temp)
 
-    # Join html
-    html = "\n".join(html)
+def load_yaml(path: str) -> Dict[str, Any]:
+    with open(path, "r") as f:
+        content = yaml.safe_load(f)
 
-    # re.sub it
-    template = re.sub(r'(?<=</header>)(.*?)(?=<footer)', html, template, flags=re.DOTALL)
+    return content
 
-    template = set_anchor_targets(template)
 
-    return template
+def main() -> None:
+    base_dir = os.path.dirname(os.path.abspath(__file__))
+    yaml_data_file = os.path.join(base_dir, "data", "content.yaml")
+    data = load_yaml(yaml_data_file)
+    data["emojis"] = EMOJIS
 
-def build_head(content, template):
-    template = re.sub(r'(?<=<title>)(.*?)(?=</title>)', content['head']['title'], template)
-    for tag in ['og_title', 'og_description', 'og_image']:
-        template = re.sub(r'(?<=<meta property="{}" content=")(.*?)(?=">)'.format(tag.replace("_", ":")), content['head'][tag], template)
-    return template
+    env = Environment(
+        loader=FileSystemLoader(os.path.join(base_dir, "templates")), autoescape=True
+    )
+    env.filters["markdown"] = markdown_filter
 
-def build_header(content, template):
-    template = re.sub(r'(?<=<h1 class=\"main-title\">)(.*?)(?=</h1>)', content['landing']['main-title'], template)
-    template = re.sub(r'(?<=<p class=\"tagline\">)(.*?)(?=</p>)', content['landing']['tagline'], template)
-    html_list = ""
-    for link in content['landing']['social_links']:
-        html_list += convert(link)
-    template = re.sub(r'(?<=<ul class=\"social-links\">)(.*?)(?=</ul>)', html_list, template, flags=re.DOTALL)
-    # Now fill in the quicklinks which links to the sections
-    sections = {
-        "About Me": "#about",
-        "Skills": "#skills",
-        "Work": "#work",
-        "Feedback": "#feedback",
-        "Projects": "#projects",
-        "Bonus Section": "#miscellaneous",
-        "Other Links": "#links"
-        }
-    html_list = ""
-    for name, link in sections.items():
-        html_list += f'<li><a href="{link}">{name}</a></li>'
-    template = re.sub(r'(?<=<ul class=\"quicklinks\">)(.*?)(?=</ul>)', html_list, template, flags=re.DOTALL)
-    return template
+    # Render template to root/index.html
+    template = env.get_template("index.html")
+    rendered = template.render(**data)
 
-def set_anchor_targets(html):
-    return re.sub(r'<a href="http', '<a target="_blank" href="http', html)
+    with open(os.path.join(base_dir, "index.html"), "w") as f:
+        f.write(rendered)
 
-if __name__ == '__main__':
-    os.chdir(os.path.abspath(os.path.dirname(__file__)))
-    data_file = os.path.join(os.getcwd(), 'data', 'content.yaml')
-    content = load_content(data_file)
-    template_file = os.path.join(os.getcwd(), 'templates', 'index_template.html')
-    template = load_template(template_file)
-    page = build_page(content, template)
-    with open(os.path.join(os.getcwd(), 'index.html'), 'w') as f:
-        f.write(page)
+
+if __name__ == "__main__":
+    main()
